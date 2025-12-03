28403222afc17f61682690d23d9a30e56c249c71
wn170298 <wali.noorzad12@gmail.com>
2025-12-03 11:29:11 +0100

    Create expemses.js

---
+++ api/expemses.js
@@ -0,0 +1,12 @@
+{
+  "name": "expense-tracker-api",
+  "version": "1.0.0",
+  "description": "Serverless API for a simple expense tracker on Vercel.",
+  "main": "api/expenses.js",
+  "scripts": {
+    "start": "vercel dev"
+  },
+  "dependencies": {
+    "nanoid": "^3.3.7"
+  }
+}
